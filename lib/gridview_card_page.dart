155a83dfad0ce844fe6e75ed4b722af882317ff8
Tensbeam <tensrrt123@gmail.com>
2022-08-25 17:07:12 +0700

    update

--- lib/gridview_card_page.dart
+++ lib/gridview_card_page.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+class Menu {
+  final String? title;
+  final IconData? icon;
+  final Color? bkColor;
+
+  Menu({this.title, this.icon, this.bkColor});
+}
+
+List<Menu> menus = [
+  Menu(title: "MENU-1", icon: Icons.person, bkColor: Colors.orangeAccent),
+  Menu(title: "MENU-2", icon: Icons.wallet, bkColor: Colors.indigo),
+  Menu(title: "MENU-3", icon: Icons.add_a_photo, bkColor: Colors.purple),
+];
+
 class GridViewCardPage extends StatelessWidget {
   const GridViewCardPage({Key? key}) : super(key: key);
 
@@ -9,25 +23,35 @@ class GridViewCardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Card Menu"),
       ),
-      body: GridView.count(
-          crossAxisCount: 3,
-          children: List.generate(6, (index) {
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: GridView.count(
+          crossAxisCount: menus.length,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          children: List.generate(menus.length, (index) {
             return Card(
-              color: Colors.amber,
+              color: menus[index].bkColor,
               elevation: 10,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.all(
-                  Radius.circular(20) 
-                )
+                  Radius.circular(20),
+                ),
               ),
               child: InkWell(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20)
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(menus[index].icon, size: 65, color: Colors.white),
+                    Text(menus[index].title!, style: TextStyle(color: Colors.white)),
+                  ],
                 ),
-                child: ,
+                onTap: () {},
               ),
             );
           }),
+        ),
       ),
     );
   }
